84170504146e16c243e456a35776c637b4157aea
Sameer Ababneh <sameer2u@gmail.com>
2017-03-28 00:03:44 +0200

    adding the www folder back as im not sure if i will be modifying it sometime later

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 rewards mobile app
 
 
-* install nodejs, angular, ionic and optionally android sdk and xcode
+* install nodejs, angular, cordova, ionic and optionally android sdk and xcode
 
 * ionic start rewards blank --v2
 * git this repo in same directory
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import {ViewChild} from 'angular2/core';
-//import { StatusBar } from '@ionic-native/status-bar';
-//import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoadingPage } from '../pages/loading/loading';
-import { NavController } from 'ionic-angular';
  
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
@@ -12,13 +8,7 @@ import { NavController } from 'ionic-angular';
 export class MyApp {
   rootPage = LoadingPage;
   constructor( platform: Platform) {
-    
-   
     platform.ready().then(() => {
-      //StatusBar.styleDefault();
-//	    splashScreen.hide();
-      // loading.dismiss();
-      
     });
   }
 }
